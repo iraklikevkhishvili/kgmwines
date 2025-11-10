d7ed23423734749577600d7b364b2288ae8fed3e
iraklikevkhishvili <iraklikevkhishvili@gmail.com>
2025-11-10 15:09:52 +0000

    Commit 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,33 @@
 import './bootstrap';
 import './modules/header';
 import './modules/accordion.js'
+
+
+
+/**
+ * Swiper Start
+ */
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+/**
+ * Swiper End
+ */
